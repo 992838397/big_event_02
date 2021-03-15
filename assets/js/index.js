61440a4726fcc867a22e5c98045149d25a81e6f6
linziyun <992838397@qq.com>
2021-03-15 20:04:50 +0800

    主页及退出登陆拦截功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,19 @@
 $(function () {
     //1.获取用户信息
     getUserInof();
+    // 2.退出功能实现
+    $('#btnLogout').on('click', function () {
+        layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function (index) {
+            // 清除本地保存的token  跳转页面
+            // 1.清空token
+            localStorage.removeItem('token');
+            // 跳转页面
+            location.href = '/login.html'
+            // 这是取消弹窗
+            layer.close(index);
+        });
+    })
+
 })
 
 // 获取用户信息(封装到入口函数的外面了)
@@ -10,18 +23,34 @@ function getUserInof() {
     $.ajax({
         // type: 'POST',
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // 身份验证！！！
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
 
         success: (res) => {
-            console.log(res);
+            // console.log(res);
             if (res.status != 0) {
                 return layui.layer.msg(res.message);
             }
             // 请求成功
             renderAvatar(res.data)
-        }
+        },
+        // 不管成不成功都执行一次
+        // 登陆 拦截
+        // complete: function (res) {
+        //     console.log(res.responseJSON);
+        //     let obj = res.responseJSON;
+        //     if (obj.status == 1 && obj.message == '身份认证失败！') {
+        //         // 删除本地token  跳转页面
+        //         // 删除token
+        //         localStorage.removeItem('token')
+        //         // 跳转页面
+        //         location.href = '/login.html'
+        //     }
+
+
+        // }
     })
 }
 function renderAvatar(user) {
@@ -30,8 +59,8 @@ function renderAvatar(user) {
     $('#welcome').html("欢迎&nbsp,&nbsp;" + name);
     //2渲染头像
     if (user.user_pic !== null) {
-        $('.layui-nav-img').show().arrt('src', 'user.user_pic');
-        $('.text.avatar').hide();
+        $('.layui-nav-img').show().attr('src', user.user_pic);
+        $('.text-avatar').hide();
 
     } else {
         // 没有头像
